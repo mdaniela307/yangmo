82649be8b62ccec6051b43a553c76fc9000fb19c
Daniela Millan Martinez <134656258+mdaniela307@users.noreply.github.com>
2024-01-26 22:37:40 -0300

    Add files via upload

---
+++ Yangmo/js/tienda.js
@@ -0,0 +1,8 @@
+var nombre_ciudad = "Valencia"
+var revisado = true
+nombre_ciudad = 32
+revisado = "no"
+var sumando1 = 23
+var sumando2 ="33"
+var suma = sumando1 + sumando2
+document.write(suma)
---
+++ Yangmo/py/tienda.py
@@ -0,0 +1,18 @@
+nombre_ciudad = "Valencia"
+revisado = True
+nombre_ciudad = 32
+print(nombre_ciudad)
+
+sumando1 = 23
+sumando2 = 33
+suma = sumando1 + sumando2
+print(suma)
+
+dia = 'nublado'
+noche = 'lluvioso'
+print(noche)
+
+if noche == 'nublado':
+    print('Está nublado')
+else:
+    print('LOL')
